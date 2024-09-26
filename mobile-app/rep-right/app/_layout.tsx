d3ac5cd2aa55b0a81ea029128d7fbb92adf5a485
redmblue <47989545+redmblue@users.noreply.github.com>
2024-09-25 21:27:32 -0400

    Added clarification comment

--- mobile-app/rep-right/app/_layout.tsx
+++ mobile-app/rep-right/app/_layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    Inter: require('../assets/fonts/Inter-VariableFont_opsz,wght.ttf'),
+    Inter: require('../assets/fonts/Inter-VariableFont_opsz,wght.ttf'), // necessary for Tamagui
   });
 
   useEffect(() => {
