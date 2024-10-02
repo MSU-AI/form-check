a8f5808a33862395d16be8e79c6e980acf7fff50
Daniel <47989545+redmblue@users.noreply.github.com>
2024-10-02 12:11:47 -0400

    Add Authentication, Sign In, Sign Up Pages, And Initial Camera Functionality (#2)
    * Add sign in screen to navigation
    
    * Added sign in page and connected firebase auth
    
    * Add sign in and sign up pages
    
    * Added tamagui
    
    * Added clarification comment
    
    * Add camera functionality
    
    * completed the sign in and sign up pages
    
    * made email and password placeholders bolder
    
    ---------
    
    Co-authored-by: Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>

--- mobile-app/rep-right/app/_layout.tsx
+++ mobile-app/rep-right/app/_layout.tsx
@@ -1,11 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import React from "react";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { createTamagui, TamaguiProvider } from "tamagui";
+import defaultConfig from "@tamagui/config/v3";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const config = createTamagui(defaultConfig);
+
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -13,7 +22,8 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Inter: require("../assets/fonts/Inter-VariableFont_opsz,wght.ttf"), // necessary for Tamagui
   });
 
   useEffect(() => {
@@ -27,11 +37,16 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <TamaguiProvider config={config}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack initialRouteName="signin">
+          <Stack.Screen name="signin" options={{ headerShown: false }} />
+          <Stack.Screen name="signup" options={{ headerShown: false }} />
+          {/* <Stack.Screen name="forgot-password" options={{ headerShown: false }} /> TODO */}
+          <Stack.Screen name="(app)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
   );
 }
