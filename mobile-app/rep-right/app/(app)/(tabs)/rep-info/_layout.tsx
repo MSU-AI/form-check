66e567066f11bf805a03fe0edf61f983349706c7
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2024-11-03 13:43:08 -0500

    nov 3 changes

---
+++ mobile-app/rep-right/app/(app)/(tabs)/rep-info/_layout.tsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import { Redirect, Stack } from "expo-router";
+
+export default function RepInfoLayout() {
+  return (
+    <Stack initialRouteName="output">
+      <Stack.Screen name="output" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
