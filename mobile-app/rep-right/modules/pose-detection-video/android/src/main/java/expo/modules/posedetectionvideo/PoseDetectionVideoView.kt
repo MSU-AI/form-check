037f8ce661165d6997b790242d653456952e8f35
Tarun Rawat <144971861+tarun05rawat@users.noreply.github.com>
2024-11-17 15:20:18 -0500

    fixed sign up and sign in

--- mobile-app/rep-right/modules/pose-detection-video/android/src/main/java/expo/modules/posedetectionvideo/PoseDetectionVideoView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.posedetectionvideo
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class PoseDetectionVideoView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
